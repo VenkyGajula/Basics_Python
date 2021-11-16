200f1d28b466d1bb69bc9da9b86485fc591d3147
VenkyGajula <94453864+VenkyGajula@users.noreply.github.com>
2021-11-16 18:35:29 +0530

    Add files via upload

--- fileHandling.py
+++ fileHandling.py
@@ -2,3 +2,18 @@ file=open("C://Users/user/Desktop/sample.txt","r")
 file1=file.read()
 print(file1)
 file.close()
+print("***newlLine*****")
+stuff='Gajula\nVenkateshwarlu'
+print(stuff)
+print(len(stuff))
+print("***Counting*****")
+count=0
+for f in file :
+    count=count+1
+    print(count)
+print("***Exercise*****")
+file=open("C://Users/user/Desktop/sample.txt","r")
+for line in file :
+    line=line.rstrip()
+    line=line.upper()
+    print(line)
